70bbb9baf41cfbf5744300b424e253c196e447b8
mclaramarinho <marinho.claramb@gmail.com>
2024-03-31 16:34:50 -0300

    feat: created the portfolio page

---
+++ FRONT/src/App.vue
@@ -0,0 +1,62 @@
+<script lang="ts">
+import NavBar from '@/components/NavBar.vue'
+import StartSection from '@/components/StartSection.vue'
+import MySkillsSection from '@/components/MySkillsSection.vue'
+import ProjectsSection from '@/components/ProjectsSection.vue'
+import ExperienceSection from '@/components/ExperienceSection.vue'
+import ContactSection from '@/components/ContactSection.vue'
+import FooterPiece from '@/components/FooterPiece.vue'
+
+import { useGoTo } from 'vuetify'
+
+export default {
+    components: {
+        NavBar,
+        StartSection,
+        MySkillsSection,
+        ProjectsSection,
+        ExperienceSection,
+        ContactSection,
+        FooterPiece
+    },
+    data() {
+        return {
+            goTo: useGoTo(),
+            scrollOptions: {
+                duration: 800,
+                easing: 'easeInOutCubic'
+            }
+        }
+    },
+    methods: {
+        goToContactSection() {
+            this.goTo('#contact_section', this.scrollOptions)
+        }
+    }
+}
+</script>
+
+<template>
+    <v-app>
+        <NavBar />
+        <main>
+            <start-section @scroll-to-contact="goToContactSection" />
+            <my-skills-section />
+            <projects-section />
+            <experience-section />
+            <contact-section />
+        </main>
+        <footer>
+            <footer-piece />
+        </footer>
+    </v-app>
+</template>
+
+<style scoped>
+main {
+    position: relative;
+    text-align: center;
+    display: grid;
+    justify-content: center;
+}
+</style>
