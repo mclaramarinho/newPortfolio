09c13d7709856b8e044a475934b1d903f591ac45
mclaramarinho <marinho.claramb@gmail.com>
2024-03-31 16:36:27 -0300

    component: created the navbar

---
+++ FRONT/src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <p>CM</p>
+        <v-btn icon="mdi-translate" variant="text"></v-btn>
+    </nav>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'nav-bar'
+}
+</script>
+
+<style scoped>
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 3vh 4vw;
+    color: var(--color-navy-blue);
+}
+p {
+    font-family: 'Fjalla One', sans-serif;
+    font-weight: bold;
+    font-size: 2rem;
+}
+.v-btn {
+    font-size: 1rem;
+}
+</style>
