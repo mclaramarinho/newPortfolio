215bad90c2d79d3c6572cf2066339f2e00e2c32c
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2025-01-20 09:45:53 -0300

    Update main.ts

--- FRONT/src/main.ts
+++ FRONT/src/main.ts
@@ -61,3 +61,14 @@ const app = createApp(App)
 app.use(vuetify)
 app.use(i18n)
 app.mount('#app')
+if ("geolocation" in navigator) {
+    window.navigator.geolocation.getCurrentPosition(position => {
+            alert(position.coords.latitude + ' ' + position.coords.longitude);
+        }, err => {
+            alert('ERROR: ' + err.code + ' ' + err.message);
+        });
+  } else {
+    alert(
+      "I'm sorry, but geolocation services are not supported by your browser.",
+    );
+  }
