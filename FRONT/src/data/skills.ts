d0fae7cdf97e65ef93ff79b86dd71a9667e04112
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2025-04-07 09:53:56 -0300

    Update skills.ts

--- FRONT/src/data/skills.ts
+++ FRONT/src/data/skills.ts
@@ -11,31 +11,53 @@ export default class Skill {
 export const skills = [
   new Skill("Vue.js", 2),
   new Skill("React.js", 2),
+  
+  new Skill("Flutter", 1),
+  new Skill("Xamarin", 2),
+  
+  new Skill("REST APIs", 1),
+  
+  new Skill("Flask", 1),
+  new Skill("FastAPI", 1),
   new Skill("Python", 2),
 
-  new Skill("MongoDB", 1),
-  new Skill("Typescript", 2),
-  new Skill("Javascript", 2),
-  new Skill("Flask", 1),
+  new Skill("C#", 2),
+  new Skill("Entity Framework", 1),
+  new Skill("Identity Framework", 1),
+  new Skill("ASP.NET MVC", 1),
+  new Skill("ASP.NET Core", 1),
+  new Skill(".NET", 2),
 
-  new Skill("FastAPI", 1),
   new Skill("OpenAI API", 1),
-  new Skill("REST APIs", 1),
+  
   new Skill("Chatbot Design", 2),
   new Skill("Blip", 1),
   new Skill("Telebot.py API", 2),
-  new Skill("UX", 2),
+  
   new Skill("Firebase Auth", 1),
   new Skill("Firebase RTDB", 2),
-  new Skill("C#", 1),
-  new Skill(".NET", 1),
+  
+  new Skill("Tailwind CSS", 1),
   new Skill("CSS", 2),
-  new Skill("JQuery", 1),
-  new Skill("Figma (Software)", 1),
   new Skill("Bootstrap", 2),
+  
+  new Skill("Typescript", 2),
+  new Skill("Javascript", 3),
+  new Skill("JQuery", 1),
+  new Skill("NPM", 1),
+  
+  new Skill("Figma (Software)", 1),
+  new Skill("UX", 2),
+  
   new Skill("GCP Cloud Functions", 1),
   new Skill("GCP Cloud Workflows", 1),
-  new Skill("Docker", 1),
+  
   new Skill("Jest", 1),
-  new Skill("NPM", 1),
+  
+  new Skill("PostgreSQL", 2),
+  new Skill("SQL Server", 1),
+  new Skill("MongoDB", 1),
+  
+  new Skill("Redlock", 1),
+  new Skill("Docker", 1),
 ];
